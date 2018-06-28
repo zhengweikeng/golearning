c51229c6c3444f1ff1bfda12b02312f664982d6f
zhengweikeng <seed_1029@outlook.com>
2018-06-28 18:31:46 +0800

    update

--- pipelinedemo/main.go
+++ pipelinedemo/main.go
@@ -1,16 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"golearning/pipeline"
+	"os"
 )
 
-func main() {
+func mergeDemo() {
 	p := pipeline.Merge(
 		pipeline.InMemSort(pipeline.ArraySource(3, 2, 6, 7, 4)),
 		pipeline.InMemSort(pipeline.ArraySource(7, 4, 0, 3, 2, 13, 8)),
 	)
 	for num := range p {
 		fmt.Println(num)
+	}
+}
+
+func main() {
+	const filename = "large.in"
+	const n = 100000000
+
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p := pipeline.RandomSource(n)
+	writer := bufio.NewWriter(file)
+	pipeline.WriterSink(writer, p)
+	writer.Flush()
+
+	file, err = os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	p = pipeline.ReaderSource(reader)
+
+	count := 1
+	for v := range p {
+		fmt.Println(v)
+		count++
+		if count > 100 {
+			break
+		}
 	}
 }
