73efca96fd2f2ca17e50e12044116209e9b08a46
zhengweikeng <seed_1029@outlook.com>
2018-06-29 19:13:13 +0800

    update

--- externalsort/main.go
+++ externalsort/main.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 	"golearning/pipeline"
 	"os"
+	"strconv"
 )
 
 func main() {
-	p := createPipeline("large.in", 800000000, 4)
-	writeToFile(p, "large.out")
-	printFile("large.out")
+	// p := createPipeline("large.in", 800000000, 4)
+	// writeToFile(p, "large.out")
+	// printFile("large.out")
+
+	p := createNetworkPipeline("small.in", 512, 4)
+	writeToFile(p, "small.out")
+	printFile("small.out")
 }
 
 func printFile(filename string) {
@@ -64,4 +69,33 @@ func writeToFile(p <-chan int, filename string) {
 	defer writer.Flush()
 
 	pipeline.WriterSink(writer, p)
+}
+
+func createNetworkPipeline(
+	filename string,
+	filesize, chunkCount int) <-chan int {
+	chunkSize := filesize / chunkCount
+	pipeline.Init()
+	sortAddr := []string{}
+
+	for i := 0; i < chunkCount; i++ {
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		file.Seek(int64(i*chunkSize), 0)
+
+		source := pipeline.ReaderSource(bufio.NewReader(file), chunkSize)
+		addr := ":" + strconv.Itoa(7000+i)
+		pipeline.NetworkSink(addr, pipeline.InMemSort(source))
+		sortAddr = append(sortAddr, addr)
+	}
+
+	sortResult := []<-chan int{}
+	for _, addr := range sortAddr {
+		sortResult = append(sortResult, pipeline.NetworkSource(addr))
+	}
+
+	return pipeline.MergeN(sortResult...)
 }
